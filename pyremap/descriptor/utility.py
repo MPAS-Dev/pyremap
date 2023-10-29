cc0ead6c41ea09a095074bfec7ce8d0030390876
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-10-29 12:04:51 +0100

    Add a function for expanding scrip corners

--- pyremap/descriptor/utility.py
+++ pyremap/descriptor/utility.py
@@ -12,6 +12,8 @@
 import sys
 
 import numpy
+import pyproj.enums
+from pyproj import Transformer
 
 
 def interp_extrap_corner(inField):
@@ -93,6 +95,82 @@ def create_scrip(outFile, grid_size, grid_corners, grid_rank, units,
     outFile.createVariable('grid_dims', 'i4', ('grid_rank',))
 
     outFile.meshName = meshName
+
+
+def expand_scrip(outFile, expandDist, expandFactor):
+    """
+    Given a SCRIP files, creates common variables and writes common values used
+    in various types of SCRIP files.
+
+    Parameters
+    ----------
+    outFile : file pointer
+        A SCRIP file opened in write mode
+
+    expandDist : float
+        A distance in meters to expand each grid cell outward from the
+        center
+
+    expandFactor : float
+        A factor by which to expand each grid cell outward from the center
+    """
+    grid_center_lat = outFile.variables['grid_center_lat'][:]
+    grid_center_lon = outFile.variables['grid_center_lon'][:]
+    grid_corner_lat = outFile.variables['grid_corner_lat'][:]
+    grid_corner_lon = outFile.variables['grid_corner_lon'][:]
+    grid_size = len(outFile.dimensions['grid_size'])
+    grid_corners = len(outFile.dimensions['grid_corners'])
+
+    radians = 'rad' in outFile.variables['grid_center_lat'].units
+
+    print(radians)
+
+    for index in range(grid_corners):
+        print(index)
+        print(grid_corner_lon[0, index], grid_corner_lat[0, index])
+
+    trans_lon_lat_to_xyz = Transformer.from_crs(4979, 4978, always_xy=True)
+    x_center, y_center, z_center = trans_lon_lat_to_xyz.transform(
+        grid_center_lon, grid_center_lat, numpy.zeros(grid_size),
+        radians=radians)
+
+    x_corner, y_corner, z_corner = trans_lon_lat_to_xyz.transform(
+        grid_corner_lon, grid_corner_lat,
+        numpy.zeros((grid_size, grid_corners)), radians=radians)
+
+    if expandFactor is None:
+        expandFactor = 1.
+
+    if expandDist is None:
+        expandDist = 0.
+
+    for index in range(grid_corners):
+        print(index)
+        print(x_corner[0, index], y_corner[0, index], z_corner[0, index])
+        print(x_center[0], y_center[0], z_center[0])
+        dx = x_corner[:, index] - x_center
+        dy = y_corner[:, index] - y_center
+        dz = z_corner[:, index] - z_center
+        print(dx[0], dy[0], dz[0])
+        dist = numpy.sqrt(dx**2 + dy**2 + dz**2)
+        print(dist[0])
+        factor = (expandFactor * dist + expandDist) / dist
+        print(factor[0])
+        x_corner[:, index] = factor * dx + x_center
+        y_corner[:, index] = factor * dy + y_center
+        z_corner[:, index] = factor * dz + z_center
+        print(x_corner[0, index], y_corner[0, index], z_corner[0, index])
+
+    grid_corner_lon, grid_corner_lat, _ = trans_lon_lat_to_xyz.transform(
+        x_corner, y_corner, z_corner, radians=radians,
+        direction=pyproj.enums.TransformDirection.INVERSE)
+
+    for index in range(grid_corners):
+        print(index)
+        print(grid_corner_lon[0, index], grid_corner_lat[0, index])
+
+    outFile.variables['grid_corner_lat'][:] = grid_corner_lat[:]
+    outFile.variables['grid_corner_lon'][:] = grid_corner_lon[:]
 
 
 def unwrap_corners(inField):
