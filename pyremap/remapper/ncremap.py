3b1b0f1590f3d0a2035326db4f33b25f32f80f57
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-04-06 11:35:57 -0600

    Reduce the complexity of _ncremap()
    Break it into multiple functions

--- pyremap/remapper/ncremap.py
+++ pyremap/remapper/ncremap.py
@@ -12,51 +12,30 @@ from pyremap.descriptor import (
 from pyremap.utility import check_call
 
 
-def _ncremap(
-    remapper,
-    in_filename,
-    out_filename,
-    variable_list,
-    overwrite,
-    renormalize,
-    logger,
-    replace_mpas_fill,
-    parallel_exec,
-):
-    """
-    Given a source file defining either an MPAS mesh or a lat-lon grid and
-    a destination file or set of arrays defining a lat-lon grid, constructs
-    a mapping file used for interpolation between the source and
-    destination grids."
-    """
-    map_filename = remapper.map_filename
-    src_descriptor = remapper.src_descriptor
-    dst_descriptor = remapper.dst_descriptor
-    if map_filename is None:
+def _validate_inputs(remapper, out_filename, overwrite):
+    if remapper.map_filename is None:
         raise ValueError('No mapping file has been defined')
-
     if not overwrite and os.path.exists(out_filename):
-        # a remapped file already exists, so nothing to do
-        return
-
-    if isinstance(src_descriptor, PointCollectionDescriptor):
+        return False  # Skip processing
+    if isinstance(remapper.src_descriptor, PointCollectionDescriptor):
         raise TypeError(
-            'Source grid is a point collection, which is notsupported.'
+            'Source grid is a point collection, which is not supported.'
         )
-
     if which('ncremap') is None:
         raise OSError(
             'ncremap not found. Make sure NCO is available in your PATH.'
         )
+    return True
 
-    if parallel_exec is not None:
-        # use the specified parallel executable
-        args = parallel_exec.split(' ')
-    else:
-        args = []
 
-    args.extend(['ncremap', '-m', map_filename, '--vrb=1'])
+def _build_args(
+    remapper, variable_list, replace_mpas_fill, renormalize, parallel_exec
+):
+    args = parallel_exec.split(' ') if parallel_exec else []
+    args.extend(['ncremap', '-m', remapper.map_filename, '--vrb=1'])
 
+    src_descriptor = remapper.src_descriptor
+    dst_descriptor = remapper.dst_descriptor
     regrid_args = []
 
     mpas_descriptors = (
@@ -68,34 +47,24 @@ def _ncremap(
         MpasCellMeshDescriptor,
         MpasEdgeMeshDescriptor,
         MpasVertexMeshDescriptor,
-        PointCollectionDescriptor
+        PointCollectionDescriptor,
     )
+
     if isinstance(src_descriptor, mpas_descriptors):
         args.extend(['-P', 'mpas'])
         if not replace_mpas_fill:
-            # the -C (climatology) flag prevents ncremap from trying to
-            # add a _FillValue attribute that might already be present
-            # and quits with an error
             args.append('-C')
-
     if isinstance(src_descriptor, MpasEdgeMeshDescriptor):
         regrid_args.extend(['--rgr col_nm=nEdges'])
-
     if (
         isinstance(src_descriptor, mpas_descriptors)
         and renormalize is not None
     ):
-        # we also want to make sure cells that receive no data are
-        # marked with fill values, even if the source MPAS data
-        # doesn't have a fill value
         args.append('--add_fill_value')
-
     if variable_list is not None:
         args.extend(['-v', ','.join(variable_list)])
-
     if renormalize is not None:
         regrid_args.append(f'--renormalize={renormalize}')
-
     if isinstance(src_descriptor, LatLonGridDescriptor):
         regrid_args.extend(
             [
@@ -110,7 +79,6 @@ def _ncremap(
                 f'--rgr lon_nm={src_descriptor.x_var_name}',
             ]
         )
-
     if isinstance(dst_descriptor, LatLonGridDescriptor):
         regrid_args.extend(
             [
@@ -130,22 +98,48 @@ def _ncremap(
             ]
         )
     elif isinstance(dst_descriptor, oned_descriptors):
-        regrid_args.extend(
-            [
-                f'--rgr col_nm={dst_descriptor.dims[0]}',
-            ]
-        )
+        dims = dst_descriptor.dims
+        assert dims is not None, "dst descriptor's dims should not be None"
+        regrid_args.extend([f'--rgr col_nm={dims[0]}'])
     if isinstance(dst_descriptor, PointCollectionDescriptor):
         regrid_args.extend(['--rgr lat_nm_out=lat', '--rgr lon_nm_out=lon'])
-
     if len(regrid_args) > 0:
         args.extend(['-R', ' '.join(regrid_args)])
+    return args
 
-    # set an environment variable to make sure we're not using czender's
-    # local version of NCO instead of one we have intentionally loaded
+
+def _set_environment():
     env = os.environ.copy()
     env['NCO_PATH_OVERRIDE'] = 'No'
+    return env
 
+
+def _ncremap(
+    remapper,
+    in_filename,
+    out_filename,
+    variable_list,
+    overwrite,
+    renormalize,
+    logger,
+    replace_mpas_fill,
+    parallel_exec,
+):
+    # Validate inputs
+    if not _validate_inputs(remapper, out_filename, overwrite):
+        # file exists and overwrite is False
+        return
+
+    # Build arguments
+    args = _build_args(
+        remapper, variable_list, replace_mpas_fill, renormalize, parallel_exec
+    )
+
+    # Set environment variables
+    env = _set_environment()
+
+    # Add input and output filenames
     args.extend([in_filename, out_filename])
 
+    # Execute the command
     check_call(args, logger=logger, env=env)
