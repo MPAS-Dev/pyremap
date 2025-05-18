b781b4b9bc38ed0216e9bf9a89768ad233458d95
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-05-18 05:08:55 -0500

    Drop grid areas for planar MPAS meshes
    We don't have a sphere radius to work from so we should just let
    the regridder handle it.

--- pyremap/descriptor/mpas_edge_mesh_descriptor.py
+++ pyremap/descriptor/mpas_edge_mesh_descriptor.py
@@ -142,10 +142,16 @@ class MpasEdgeMeshDescriptor(MeshDescriptor):
             mask = cells_on_edge[:, i_cell] >= 0
             valid_cells_on_edge[mask] = valid_cells_on_edge[mask] + 1
 
-        ds_out['grid_area'] = (
-            ('grid_size',),
-            0.5 * valid_cells_on_edge * dc_edge * dv_edge / (sphere_radius**2),
-        )
+        if sphere_radius > 0:
+            ds_out['grid_area'] = (
+                ('grid_size',),
+                0.5
+                * valid_cells_on_edge
+                * dc_edge
+                * dv_edge
+                / (sphere_radius**2),
+            )
+            ds_out.grid_area.attrs['units'] = 'radians^2'
 
         ds_out['grid_center_lat'] = (('grid_size',), lat_edge)
         ds_out['grid_center_lon'] = (('grid_size',), lon_edge)
@@ -198,7 +204,6 @@ class MpasEdgeMeshDescriptor(MeshDescriptor):
         ds_out.grid_corner_lat.attrs['units'] = 'radians'
         ds_out.grid_corner_lon.attrs['units'] = 'radians'
         ds_out.grid_imask.attrs['units'] = 'unitless'
-        ds_out.grid_area.attrs['units'] = 'radians^2'
 
         if expand_dist is not None or expand_factor is not None:
             expand_scrip(ds_out, expand_dist, expand_factor)
